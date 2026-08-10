7a55424324c1028e2ab1032b716d76098a02d84f
Fahmy Mahmoud <fahmy.14.mahmoud@gmail.com>
2026-08-10 11:21:21 +0300

    Add favorites use cases.

---
+++ domain/src/main/java/com/linkdevelopment/domain/usecase/AddFavoriteUseCase.kt
@@ -0,0 +1,13 @@
+package com.linkdevelopment.domain.usecase
+
+import com.linkdevelopment.domain.model.Meal
+import com.linkdevelopment.domain.repository.RecipeRepository
+import javax.inject.Inject
+
+class AddFavoriteUseCase @Inject constructor(
+    private val recipeRepository: RecipeRepository
+) {
+    suspend operator fun invoke(meal: Meal) {
+        recipeRepository.addFavorite(meal)
+    }
+}
