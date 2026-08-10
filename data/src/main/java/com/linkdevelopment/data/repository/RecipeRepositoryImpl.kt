ac92de8f342afdbb99ac66d9369d6402796613e9
Fahmy Mahmoud <fahmy.14.mahmoud@gmail.com>
2026-08-10 11:17:20 +0300

    Implement favorites repository.

--- data/src/main/java/com/linkdevelopment/data/repository/RecipeRepositoryImpl.kt
+++ data/src/main/java/com/linkdevelopment/data/repository/RecipeRepositoryImpl.kt
@@ -1,13 +1,18 @@
 package com.linkdevelopment.data.repository
 
+import com.linkdevelopment.data.local.entity.FavoriteMealEntity
+import com.linkdevelopment.data.local.localdatasource.FavoriteMealsLocalDataSource
 import com.linkdevelopment.data.mapper.toMeal
 import com.linkdevelopment.data.remote.api.MealApiService
 import com.linkdevelopment.domain.model.Meal
 import com.linkdevelopment.domain.repository.RecipeRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RecipeRepositoryImpl @Inject constructor(
-    private val mealApiService: MealApiService
+    private val mealApiService: MealApiService,
+    private val favoriteMealsLocalDataSource: FavoriteMealsLocalDataSource
 ) : RecipeRepository {
 
     override suspend fun getRecipesByCategory(category: String): List<Meal> {
@@ -31,4 +36,45 @@ class RecipeRepositoryImpl @Inject constructor(
             ?.mapNotNull { it.name }
             ?: emptyList()
     }
+
+    private fun Meal.toFavoriteEntity(): FavoriteMealEntity {
+        return FavoriteMealEntity(
+            id = id,
+            name = name,
+            imageUrl = imageUrl,
+            category = category,
+            area = area
+        )
+    }
+
+    override suspend fun addFavorite(meal: Meal) {
+        favoriteMealsLocalDataSource.addFavorite(
+            meal.toFavoriteEntity()
+        )
+    }
+
+    override suspend fun removeFavorite(meal: Meal) {
+        favoriteMealsLocalDataSource.removeFavorite(
+            meal.toFavoriteEntity()
+        )
+    }
+
+    override fun getFavorites(): Flow<List<Meal>> {
+        return favoriteMealsLocalDataSource
+            .getFavorites()
+            .map { meals ->
+                meals.map { entity ->
+                    Meal(
+                        id = entity.id,
+                        name = entity.name,
+                        imageUrl = entity.imageUrl,
+                        category = entity.category,
+                        area = entity.area
+                    )
+                }
+            }
+    }
+
+
+
 }
--- domain/src/main/java/com/linkdevelopment/domain/repository/RecipeRepository.kt
+++ domain/src/main/java/com/linkdevelopment/domain/repository/RecipeRepository.kt
@@ -1,6 +1,7 @@
 package com.linkdevelopment.domain.repository
 
 import com.linkdevelopment.domain.model.Meal
+import kotlinx.coroutines.flow.Flow
 
 interface RecipeRepository {
 
@@ -9,6 +10,13 @@ interface RecipeRepository {
     suspend fun searchRecipes(query: String): List<Meal>
 
     suspend fun getCategories(): List<String>
+
+
+    suspend fun addFavorite(meal: Meal)
+
+    suspend fun removeFavorite(meal: Meal)
+
+    fun getFavorites(): Flow<List<Meal>>
 
 
 }
